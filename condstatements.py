f7d2133d5d9413cb1126c8171007adc43cd527cd
Sankhya Londhe <londhesankhya2005@gmail.com>
2025-09-15 16:40:23 +0530

    Add files via upload

---
+++ condstatements.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+#if n%2 == 0:
+#   print("is divisible")
+#elif n%3 == 0:
+#    print("is not even yet divisible by 3")
+#else:
+#   print("is not divisible")
+
+
+# if n%2 == 0:
+#    print("is divisible")
+# elif n%3 == 0:
+#    print("is not even yet divisible by 3")
+# else:
+#    print("is not divisible")
+
